e92efd6d2d732ec96976f23bc92118390724e594
cliber castillo <cliber.castillo.rp@viaexperis2.pe>
2023-05-16 09:46:03 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace votacion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
